2b4e2a5363f53ae4306860aff8ed4f4d6b299b9f
alexzhangyu <zhangy41@rpi.edu>
2018-08-01 05:44:52 -0400

    minor fix

--- phSolver/compressible/print_interface_error_data_m.f
+++ phSolver/compressible/print_interface_error_data_m.f
@@ -7,8 +7,10 @@ c
         real*8, dimension(5) :: int_err_if_flux  ! surface intgeral of error for flux
         real*8, dimension(5) :: error_if_tan     ! surface averaged error for tangential quantities
         real*8, dimension(5) :: int_err_if_tan   ! surface intgeral of error for tangential quantities
+        real*8 :: int_area                       ! area of interface
 c
         real*8, dimension(:,:), allocatable :: int_err_if_flux_blk  ! surface intgeral of error for flux at blk level
         real*8, dimension(:,:), allocatable :: int_err_if_tan_blk   ! surface intgeral of error for tangential quantities at blk level
+        real*8, dimension(:), allocatable :: int_area_blk
 c      
       end module print_interface_error_data_m
