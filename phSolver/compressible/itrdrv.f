b259419cbc5b99615237aca91321aa02530be6b4
alexzhangyu <zhangy41@rpi.edu>
2020-03-24 22:20:34 -0400

    Reverting the local change to the sphere problem

--- phSolver/compressible/itrdrv.f
+++ phSolver/compressible/itrdrv.f
@@ -602,6 +602,12 @@ c                        write(*,*) 'lhs=',lhs
      &                       shpif,         shgif,
      &                       solinc,        rerr,          umesh)
 c
+                      call set_if_velocity (BC,     iBC,     umesh,
+     &                                      disp,   x,       Delt(1),
+     &                                      ilwork, nshg,    ndofBC,
+     &                                      nsd,    nelblif, nlwork,
+     &                                      ndof)
+c
                     endif
                       else if (mod(impl(1),100)/10 .eq. 2) then ! mfg solve
 c     
@@ -712,12 +718,6 @@ c
 c
 c.... update interface mesh bc based on umesh
 c.... we should have correct umesh at this point
-c... hack when to update the interface velocity
-                     call set_if_velocity (BC,  iBC, 
-     &                                umesh,    disp, x,  Delt(1),   ilwork,
-     &                                nshg,  ndofBC,
-     &                                nsd,   nelblif, nlwork, ndof )
-c
 c
                     do inode = 1, nshg
                       if ( ifFlag(inode) .eq. 1 ) then
