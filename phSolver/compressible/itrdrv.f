5126872370a83b0012d4d19d2ae75f901c0c921f
Anirban <anirbanchandra1@gmail.com>
2020-04-01 22:08:55 -0400

    rstatCheck correction and interface velocity call correction

--- phSolver/compressible/itrdrv.f
+++ phSolver/compressible/itrdrv.f
@@ -610,11 +610,6 @@ c                        write(*,*) 'lhs=',lhs
      &                       shpif,         shgif,
      &                       solinc,        rerr,          umesh)
 c
-                      call set_if_velocity (BC,     iBC,     umesh,
-     &                                      disp,   x,       Delt(1),
-     &                                      ilwork, nshg,    ndofBC,
-     &                                      nsd,    nelblif, nlwork,
-     &                                      ndof)
 c
                     endif
                       else if (mod(impl(1),100)/10 .eq. 2) then ! mfg solve
